47f7f8fe7d59f875ec2155898e936d2aaacf589b
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-05-20 16:11:53 +0200

    comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,9 @@ export const metadata: Metadata = {
     title: "DP's Templates",
     description: "Effortlessly integrate a library of ready-to-use SwiftUI views and Next.js UI components...",
     type: "website",
-    images: [{
-      url: "https://dps-template.vercel.app/metadata.png",  // Full absolute URL
-      width: 1200,
-      height: 630,
-      alt: "DP's Templates Preview",
-    }],
+    images: [
+      "https://dps-template.vercel.app/metadata.png",  // Full absolute URL
+    ],
   },
 };
 
