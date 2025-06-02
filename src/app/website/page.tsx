f3f083ed86a6cbe4fb9ee0026e8d6be04bbede14
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-06-02 17:24:31 +0200

    paddin in notfound

--- src/app/website/page.tsx
+++ src/app/website/page.tsx
@@ -22,17 +22,17 @@ const page = () => {
   return (
     <div
       className="bg-black w-full
-      bg-cover bg-center
+      bg-cover bg-center overflow-x-hidden
       bg-[linear-gradient(rgba(0,0,0,1.0),rgba(0,0,0,0.85)),url('/gridd.png')] 
       flex justify-start p-6 md:p-12 items-center min-h-screen relative text-white"
     >
       <LightGradient />
       <div className="z-100 fixed flex w-full items-center justify-between p-6 sm:py-6 sm:px-12 mb-3 top-0 left-1/2 transform -translate-x-1/2 bg-transparent backdrop-blur-lg border-b border-neutral-800 shadow-lg m-auto">
-        <NavbarWebsite/>
+        <NavbarWebsite />
       </div>
-      <div className="flex flex-col relative top-20 xl:top-0 w-full xl:flex-row justify-between">
-        <div className="flex w-fit flex-col gap-y-5">
-          <WhatsNewButton/>
+      <div className="flex flex-col relative top-20 xl:top-0 w-full max-w-full xl:flex-row justify-between">
+        <div className="flex w-full xl:w-fit flex-col gap-y-5">
+          <WhatsNewButton />
           <div className=" w-fit">
             <HeroSectionText />
           </div>
@@ -41,12 +41,12 @@ const page = () => {
             pixel-perfect UIs in minutes without having to worry about styling
             and animations.
           </p>
-          <Buttons/>
+          <Buttons />
           <Frameworks />
         </div>
 
-        <div className="flex  border-l border-r border-dashed border-neutral-800 gap-5 flex-col">
-          <div className="relative min-h-screen lg:h-full w-full divide-x divide-dashed divide-neutral-800 grid-cols-1 gap-5 items-start lg:grid lg:grid-cols-2">
+        <div className="flex h-full border-l border-r border-dashed border-neutral-800 gap-5 flex-col">
+          <div className="relative w-full divide-x divide-dashed divide-neutral-800 grid-cols-1 gap-5 items-start lg:grid lg:grid-cols-2">
             <div className="relative p-5 grid divide-y divide-dashed divide-neutral-800 w-full gap-5">
               <div className="flex items-center border-b pb-5 border-dashed border-neutral-800 gap-5">
                 <IOSToggle />
@@ -64,8 +64,10 @@ const page = () => {
                 <HoverCard3D />
               </div>
             </div>
-            <div className="relative grid divide-y pr-5 pl-5 lg:pl-0  divide-dashed divide-neutral-800 w-full gap-5">
-              <ToolTipLinkImage />
+            <div className="relative grid divide-y pr-5 pl-5 lg:pl-0 divide-dashed divide-neutral-800 w-full gap-5">
+              <div className="">
+                <ToolTipLinkImage />
+              </div>
               <div className="h-full w-80">
                 <BackgroundBeamsWithCollisionDemo />
               </div>
