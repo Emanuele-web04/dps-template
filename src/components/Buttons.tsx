144737b2e6bb16bd6e46b3d33ab0bab8e3c20e97
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-06-03 15:22:36 +0200

    paddin in notfound

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Buttons = () => {
   return (
     <div className="flex md:flex-row flex-col w-full md:w-fit gap-5">
-      <button className="font-inter p-8 py-5 rounded-2xl cursor-pointer bg-white text-black text-sm font-medium tracking-tighter">
+      <button className="font-inter p-8 py-4 md:py-5 rounded-2xl cursor-pointer bg-white text-black text-sm font-medium tracking-tighter">
         <p>Browse Templates</p>
       </button>
-      <button className="font-inter p-8 py-5 rounded-2xl bg-transparent cursor-pointer text-white ring-1 ring-neutral-600 text-sm font-medium tracking-tighter">
+      <button className="font-inter p-8 py-4 md:py-5 rounded-2xl bg-transparent cursor-pointer text-white ring-1 ring-neutral-600 text-sm font-medium tracking-tighter">
         <p>Browse Components</p>
       </button>
     </div>
