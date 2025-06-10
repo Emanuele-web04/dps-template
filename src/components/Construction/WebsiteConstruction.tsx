79536593e41c9bd41d3c579380bcfd36dce9aa7c
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-06-11 00:45:56 +0200

    light mode

--- src/components/Construction/WebsiteConstruction.tsx
+++ src/components/Construction/WebsiteConstruction.tsx
@@ -8,7 +8,7 @@ import { TbTools } from "react-icons/tb";
 import ItalyClock from "./CurrentTime";
 
 const WebsiteInProduction = () => {
-  const progress = 14;
+  const progress = 20;
 
   return (
     <div className="min-h-screen font-inter tracking-tighter flex items-center flex-col justify-center relative overflow-hidden">
