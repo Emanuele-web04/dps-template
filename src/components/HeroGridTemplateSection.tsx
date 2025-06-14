d252ec5891e097848211a920e22138b4d4ca43fd
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-06-14 18:39:18 +0200

    light mode

--- src/components/HeroGridTemplateSection.tsx
+++ src/components/HeroGridTemplateSection.tsx
@@ -11,7 +11,7 @@ const HeroGridTemplateSection = ({
       {/* Content */}
       <div className="relative z-20">
         <h1 className="title-font grid-title text-center!">Templates</h1>
-        <p className="my-6 text-sm md:text-lg text-center font-normal text-neutral-600 dark:text-neutral-300 mx-auto max-w-3xl leading-relaxed">
+        <p className="my-6 font-inter tracking-tight text-sm md:text-lg text-center font-normal text-neutral-600 dark:text-neutral-300 mx-auto max-w-3xl leading-relaxed">
           Modern and minimalist templates for building your next product. Built
           with React, Next.js, Tailwind CSS, Framer Motion and TypeScript.
         </p>
