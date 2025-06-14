27288d40fd5389006290492f11572cc5a708569a
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-06-14 18:30:54 +0200

    light mode

--- src/components/DropdownMobile.tsx
+++ src/components/DropdownMobile.tsx
@@ -27,13 +27,13 @@ const DropdownMobile = () => {
       </button>
       <span
         className={cn(
-          "absolute -bottom-40 max-w-full! sm:w-80 w-50 justify-start items-start bg-white dark:bg-neutral-900 transition-all rounded-xl ring-1 ring-neutral-200 dark:ring-neutral-800",
+          "absolute -bottom-40 max-w-full! sm:w-80 w-50 justify-start items-start bg-white dark:bg-neutral-900 transition-all rounded-xl ring-1 ring-neutral-200 dark:ring-neutral-700",
           open
             ? "opacity-100 scale-100 shadow-lg dark:shadow-black/40"
             : "opacity-0 scale-0 cursor-none pointer-events-none"
         )}
       > 
-        <div className="flex divide-y items-start divide-neutral-200 flex-col">
+        <div className="flex divide-y items-start divide-neutral-200 dark:divide-neutral-700 flex-col">
           {templates.map((t, i) => (
             <Link key={i} className="w-full text-left" href={`/templates/${t.slug}`}>
               <p className="font-inter tracking-tight hover:text-black p-2 dark:hover:text-white dark:text-neutral-300 text-neutral-800 text-sm">
