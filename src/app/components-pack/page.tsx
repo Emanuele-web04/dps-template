6eab249cf72d721fe05daea2e7a32497b1988476
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-06-16 01:00:58 +0200

    light mode

--- src/app/components-pack/page.tsx
+++ src/app/components-pack/page.tsx
@@ -1,10 +1,6 @@
 import HeroGridTemplateSection from "@/components/HeroGridTemplateSection";
-import { templates } from "@/lib/templates";
-import ShowcaseTemplate from "@/components/ShowcaseTemplate";
-import DropdownMobile from "@/components/DropdownMobile";
 import Aside from "@/components/Aside";
 import { components } from "@/lib/components";
-import Link from "next/link";
 import ShowcaseComponent from "@/components/ShowcaseComponent";
 import DropdownMobileComponent from "@/components/DropdownMobileComponent";
 
@@ -25,7 +21,7 @@ const Page = () => {
           {/* Sidebar */}
           <Aside />
           {/* Main content */}
-          <div className="col-span-1 lg:col-span-9 mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="col-span-1 fade-in-up lg:col-span-9 mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {components.map((c, i) => (
               <ShowcaseComponent key={i} {...c} />
             ))}
--- src/components/ShowcaseComponent.tsx
+++ src/components/ShowcaseComponent.tsx
@@ -1,6 +1,7 @@
 import { ComponentType } from "@/types/component";
 import Image from "next/image";
-const ShowcaseComponent = ({ title, desc, price }: ComponentType) => {
+import StackIcons from "./ui/stack-icons";
+const ShowcaseComponent = ({ title, desc, price, stack }: ComponentType) => {
   return (
     <div className="p-1.5 rounded-2xl ring-1 ring-neutral-200 dark:bg-neutral-950 dark:ring-neutral-800 bg-neutral-100">
       <div className="col-span-1 group cursor-pointer font-inter tracking-tight hover:bg-neutral-100 bg-neutral-50 dark:hover:bg-neutral-950 dark:bg-neutral-900 overflow-clip rounded-2xl ring-1 ring-neutral-200 dark:ring-neutral-800">
@@ -13,7 +14,7 @@ const ShowcaseComponent = ({ title, desc, price }: ComponentType) => {
             height={1000}
           />
         </div>
-        <div className="flex flex-col text-primary p-4 w-full gap-y-2">
+        <div className="flex flex-col text-primary p-4 w-full gap-y-3">
           <div className="flex w-full justify-between items-center">
             <div className="font-semibold text-lg">{title}</div>
             <div className="flex items-center gap-x-3">
@@ -26,6 +27,7 @@ const ShowcaseComponent = ({ title, desc, price }: ComponentType) => {
           <p className="text-neutral-600 dark:text-neutral-400 text-sm">
             {desc}
           </p>
+          <StackIcons stack={stack} />
         </div>
       </div>
     </div>
