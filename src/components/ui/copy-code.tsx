02a85ca726b37b490a061141292477ec68bce5c9
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-06-19 21:06:07 +0200

    light mode

--- src/components/ui/copy-code.tsx
+++ src/components/ui/copy-code.tsx
@@ -27,11 +27,31 @@ const CopyCode = ({
   };
 
   return (
-    
+    <div className="relative">
+      <div
+        className={cn(
+          "text-primary absolute top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 z-10",
+          copy
+            ? "opacity-100 scale-100"
+            : "scale-0 opacity-0 pointer-events-none"
+        )}
+      >
+        <div className="flex gap-x-1 px-3 py-2 bg-black dark:bg-white rounded-xl sm:text-base text-sm items-center">
+          <TbCircleCheckFilled className="text-emerald-500" />
+          <p className="text-white font-medium dark:text-black">Copied!</p>
+        </div>
+      </div>
+      <div 
+        className=" rounded-xl ring-1 max-h-130 ring-neutral-200 dark:ring-neutral-800 overflow-hidden flex flex-col bg-neutral-50 dark:bg-neutral-900 cursor-pointer"
+        onClick={handleCopy}
+      >
+        <div className="overflow-clip code-scrollbar">
           <Code className={language} color={color}>
             {code}
           </Code>
-       
+        </div>
+      </div>
+    </div>
   );
 };
 
