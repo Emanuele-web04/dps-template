6caa887cc53aa1170afd17c80ba0481c1c2c132e
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-07-01 00:26:14 +0200

    plus

--- src/components/ui/main-hero/plus.tsx
+++ src/components/ui/main-hero/plus.tsx
@@ -1,21 +1,39 @@
-import { PiPlusThin } from "@/utils/icons";
 import React from "react";
 
 const Plus = () => {
   return (
     <>
-      <span className="inline-flex absolute -top-3 -left-3     text-neutral-200 dark:text-neutral-700">
-        <PiPlusThin className="text-2xl" />
-      </span>
-      <span className="inline-flex absolute -top-3 -right-3  text-neutral-200 dark:text-neutral-700">
-      <PiPlusThin    className="text-2xl" />
-      </span>
-      <span className="inline-flex absolute -bottom-3 -left-3    text-neutral-200 dark:text-neutral-700">
-        <PiPlusThin className="text-2xl" />
-      </span>
-      <span className="inline-flex absolute -bottom-3 -right-3   text-neutral-200 dark:text-neutral-700 ">
-        <PiPlusThin className="text-2xl" />
-      </span>
+      {/* Top-left plus */}
+      <div className="absolute -top-[8.3px] -left-[8.3px] w-4 h-4 pointer-events-none">
+        {/* Horizontal line */}
+        <div className="absolute top-1/2 left-0 right-0 h-px bg-neutral-200 dark:bg-neutral-700 transform -translate-y-1/2"></div>
+        {/* Vertical line */}
+        <div className="absolute left-1/2 top-0 bottom-0 w-px bg-neutral-200 dark:bg-neutral-700 transform -translate-x-1/2"></div>
+      </div>
+
+      {/* Top-right plus */}
+      <div className="absolute -top-[8.3px] -right-[8.3px] w-4 h-4 pointer-events-none">
+        {/* Horizontal line */}
+        <div className="absolute top-1/2 left-0 right-0 h-px bg-neutral-200 dark:bg-neutral-700 transform -translate-y-1/2"></div>
+        {/* Vertical line */}
+        <div className="absolute left-1/2 top-0 bottom-0 w-px bg-neutral-200 dark:bg-neutral-700 transform -translate-x-1/2"></div>
+      </div>
+
+      {/* Bottom-left plus */}
+      <div className="absolute -bottom-[8.3px] -left-[8.3px] w-4 h-4 pointer-events-none">
+        {/* Horizontal line */}
+        <div className="absolute top-1/2 left-0 right-0 h-px bg-neutral-200 dark:bg-neutral-700 transform -translate-y-1/2"></div>
+        {/* Vertical line */}
+        <div className="absolute left-1/2 top-0 bottom-0 w-px bg-neutral-200 dark:bg-neutral-700 transform -translate-x-1/2"></div>
+      </div>
+
+      {/* Bottom-right plus */}
+      <div className="absolute -bottom-[8.3px] -right-[8.3px] w-4 h-4 pointer-events-none">
+        {/* Horizontal line */}
+          <div className="absolute top-1/2 left-0 right-0 h-px bg-neutral-200 dark:bg-neutral-700 transform -translate-y-1/2"></div>
+        {/* Vertical line */}
+        <div className="absolute left-1/2 top-0 bottom-0 w-px  bg-neutral-200 dark:bg-neutral-700 transform -translate-x-1/2"></div>
+      </div>
     </>
   );
 };
